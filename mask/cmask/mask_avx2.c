fe208a77d0fab131b6a88b129687f8babb918ad6
guonaihong <guonaihong@qq.com>
2024-05-03 11:09:52 +0800

    更新

--- mask/cmask/mask_avx2.c
+++ mask/cmask/mask_avx2.c
@@ -71,17 +71,152 @@ static INLINE void mask_8(uint8_t *payload, size_t size, uint32_t key) {
     *(*uit64_t)(payload) = key64;
 }
 
+// 一次处理2字节
+static INLINE void mask_avx2_2(uint8_t *payload, size_t size, uint32_t key) {
+    *(uint16_t *)payload = (uint16_t)key;
+}
+
+// 一次处理1字节
+static INLINE void mask_avx2_1(uint8_t *payload, size_t size, uint32_t key) {
+    *payload = (uint8_t)key;
+}
+
 // 1-255大小的处理
 static INLINE void mask_tiny(uint8_t *payload, size_t size, uint32_t key, __m256i key256) {
-    // Handle the remaining data
-    size_t remaining = size & ~(size_t)0x1F;
-    switch (size - remaining) {
+    switch (size) {
         case 0: break;
-        case 16: mask_avx2_128(payload + remaining, 16, key); break;
-        case 32: mask_avx2(payload + remaining, 32, key); break;
-        case 64: mask_avx2_64(payload + remaining, 64, key); break;
-        case 128: mask_avx2_128(payload + remaining, 128, key); break;
-        default: mask_tiny(payload + remaining, size - remaining, key);
+        case 128: mask_avx2_128(payload, size, key256); break;
+        case 64: mask_avx2_64(payload, size, key256);
+        case 32: mask_avx2_32(payload, size, key256); break;
+        case 96: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_32(payload + 64, 32, key256); break;
+        case 16: mask_avx2_16(payload, size, key); break;
+        case 48: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_16(payload + 32, 16, key); break;
+        case 80: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_16(payload + 64, 16, key); break;
+        case 112: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key); break;
+        case 8: mask_avx2_8(payload, size, key); break;
+        case 24: mask_avx2_16(payload, 16, key);
+                 mask_avx2_8(payload + 16, 8, key); break;
+        case 40: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_8(payload + 32, 8, key); break;
+        case 56: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_16(payload + 32, 16, key);
+                 mask_avx2_8(payload + 48, 8, key); break;
+        case 72: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_8(payload + 64, 8, key); break;
+        case 88: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_16(payload + 64, 16, key);
+                 mask_avx2_8(payload + 80, 8, key); break;
+        case 104: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_8(payload + 96, 8, key); break;
+        case 120: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key); break;
+        case 4: mask_avx2_4(payload, size, key); break;
+        case 20: mask_avx2_16(payload, 16, key);
+                 mask_avx2_4(payload + 16, 4, key); break;
+        case 36: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_4(payload + 32, 4, key); break;
+        case 52: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_16(payload + 32, 16, key);
+                 mask_avx2_4(payload + 48, 4, key); break;
+        case 68: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_4(payload + 64, 4, key); break;
+        case 84: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_16(payload + 64, 16, key);
+                 mask_avx2_4(payload + 80, 4, key); break;
+        case 100: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_4(payload + 96, 4, key); break;
+        case 116: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_4(payload + 112, 4, key); break;
+        case 124: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_4(payload + 120, 4, key); break;
+        case 2: mask_avx2_2(payload, size, key); break;
+        case 18: mask_avx2_16(payload, 16, key);
+                 mask_avx2_2(payload + 16, 2, key); break;
+        case 34: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_2(payload + 32, 2, key); break;
+        case 50: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_16(payload + 32, 16, key);
+                 mask_avx2_2(payload + 48, 2, key); break;
+        case 66: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_2(payload + 64, 2, key); break;
+        case 82: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_16(payload + 64, 16, key);
+                 mask_avx2_2(payload + 80, 2, key); break;
+        case 98: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_32(payload + 64, 32, key256);
+                 mask_avx2_2(payload + 96, 2, key); break;
+        case 114: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_2(payload + 112, 2, key); break;
+        case 122: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_2(payload + 120, 2, key); break;
+        case 1: mask_avx2_1(payload, size, key); break;
+        case 17: mask_avx2_16(payload, 16, key);
+                 mask_avx2_1(payload + 16, 1, key); break;
+        case 33: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_1(payload + 32, 1, key); break
+                 case 49: mask_avx2_32(payload, 32, key256);
+                 mask_avx2_16(payload + 32, 16, key);
+                 mask_avx2_1(payload + 48, 1, key); break;
+        case 65: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_1(payload + 64, 1, key); break;
+        case 81: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_16(payload + 64, 16, key);
+                 mask_avx2_1(payload + 80, 1, key); break;
+        case 97: mask_avx2_64(payload, 64, key256);
+                 mask_avx2_32(payload + 64, 32, key256);
+                 mask_avx2_1(payload + 96, 1, key); break;
+        case 113: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_1(payload + 112, 1, key); break;
+        case 121: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_1(payload + 120, 1, key); break;
+        case 123: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_2(payload + 120, 2, key);
+                  mask_avx2_1(payload + 122, 1, key); break;
+        case 125: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_4(payload + 120, 4, key);
+                  mask_avx2_1(payload + 124, 1, key); break;
+        case 127: mask_avx2_64(payload, 64, key256);
+                  mask_avx2_32(payload + 64, 32, key256);
+                  mask_avx2_16(payload + 96, 16, key);
+                  mask_avx2_8(payload + 112, 8, key);
+                  mask_avx2_4(payload + 120, 4, key);
+                  mask_avx2_2(payload + 124, 2, key);
+                  mask_avx2_1(payload + 126, 1, key); break;
+        default:
+            assert(0 && "Unsupported size in mask_tiny");
+            // for (size_t i = 0; i < size; i++) {
+            //     payload[i] ^= (uint8_t)key;
+            // }
     }
 }
 
